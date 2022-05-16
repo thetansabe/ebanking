8d2fe5b87f5f46fd5128bfafef5d0c3da6680154
thetansabe <thetannguyen193@gmail.com>
2022-05-16 16:22:19 +0700

    front phase1

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+/////////////////////////PROFILE
+const profile = document.querySelector('.account_content')
+if(profile){
+    function handleUpdateInfo(icon){
+        console.log(icon.parentElement.innerText)
+        //return icon.parentElement.innerText
+    }   
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('dashboard');
 });
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile');
+});
+
+router.get('/transfer', (req, res) => {
+  res.render('transfer')
+})
 
 module.exports = router;
